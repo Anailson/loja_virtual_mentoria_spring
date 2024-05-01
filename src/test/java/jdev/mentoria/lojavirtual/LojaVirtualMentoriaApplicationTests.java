e0bd92a39b46fa3ef8ee6b542a4a5e2a297e42b5
anailson <arsantos315@gmail.com>
2024-05-01 10:28:50 -0300

    Feature: Criando uma classe de teste para testa a Model(acesso)em conjunto com a controller

--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -1,13 +1,28 @@
 package jdev.mentoria.lojavirtual;
 
+import jdev.mentoria.lojavirtual.controller.AcessoController;
+import jdev.mentoria.lojavirtual.model.Acesso;
+import jdev.mentoria.lojavirtual.repository.AcessoRepository;
+import jdev.mentoria.lojavirtual.service.AcessoService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.UpperCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LojaVirtualMentoriaApplicationTests {
+@SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
+public class LojaVirtualMentoriaApplicationTests {
+
+	@Autowired
+	private AcessoController acessoController;
 
 	@Test
-	void contextLoads() {
+	public void testCadastraAcesso() {
+
+		Acesso acesso = new Acesso();
+
+		acesso.setDescricao("ROLE_ADMIN");
+
+		acessoController.salvarAcesso(acesso);
 	}
 
 }
