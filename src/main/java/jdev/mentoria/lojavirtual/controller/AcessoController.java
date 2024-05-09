0a0b3c64e6cfc4b10c0c226546cdd080979f308a
anailson <arsantos315@gmail.com>
2024-05-09 19:35:29 -0300

    feature: Liberando acesso no websecurity - Realizando alguns testes com Junit e teste Query de acesso do repository

--- src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
+++ src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
@@ -2,20 +2,39 @@ package jdev.mentoria.lojavirtual.controller;
 
 
 import jdev.mentoria.lojavirtual.model.Acesso;
+import jdev.mentoria.lojavirtual.repository.AcessoRepository;
 import jdev.mentoria.lojavirtual.service.AcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RestController
 public class AcessoController {
 
     @Autowired
     private AcessoService acessoService;
 
-    @PostMapping("/salvarAcesso")
-    public Acesso salvarAcesso(Acesso acesso){
+    @Autowired
+    private AcessoRepository acessoRepository;
 
-        return acessoService.save(acesso);
+    @ResponseBody  /*Retorno da API*/
+    @PostMapping(value = "/salvarAcesso") /*Mapeando a URL para receber JSON*/
+    public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso){/*REceber o JSON e converte para objeto*/
+
+        Acesso acessoSalvo = acessoService.save(acesso);
+
+        return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+    }
+
+    @ResponseBody  /*Retorno da API*/
+    @PostMapping(value = "/deleteAcesso") /*Mapeando a URL para receber JSON*/
+    public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso){/*REceber o JSON e converte para objeto*/
+
+        acessoRepository.deleteById(acesso.getId());
+
+        return new ResponseEntity("Acesso removido",HttpStatus.OK);
     }
 }
